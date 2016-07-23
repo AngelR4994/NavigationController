d28a1261a604e4ac10a5e9de8fb73e3a8b49b2d6
xax <xax@xaxs-mac.local>
2016-07-23 08:25:05 -0500

    Initial Commit

---
+++ Navigation Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Navigation Controller
+//
+//  Created by xax on 7/23/16.
+//  Copyright © 2016 xax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Navigation Controller/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Navigation Controller
+//
+//  Created by xax on 7/23/16.
+//  Copyright © 2016 xax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
